fbecf635ce070d89695085fc54bdc7b4fe23e947
OllieGuy <ollieguy02@gmail.com>
2023-05-02 16:51:12 +0100

    chapter 2 foundations

--- Assets/Scripts/Chapter 2/Chapter2Controller.cs
+++ Assets/Scripts/Chapter 2/Chapter2Controller.cs
@@ -1,46 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class Chapter2Controller : MonoBehaviour
 {
-    KeyController KC = new KeyController();
     public AudioManager am;
-    public Camera firstCam;
     public int level = 0;
-    int unacceptableKeyCounter = 0;
+    public GameObject player;
+    public Camera platformCamera;
+    public Camera walledCamera;
+    public Camera fpCamera;
+    private Rigidbody rb;
+    private Player3rdPersonController p3c;
+    private Player1stPersonMovement p1m;
 
     void Start()
     {
-        //firstCam.enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        walledCamera.enabled = false;
+        fpCamera.enabled = false;
+        platformCamera.enabled = true;
+        rb = player.GetComponent<Rigidbody>();
+        p3c = player.GetComponent<Player3rdPersonController>();
+        p1m = player.GetComponent<Player1stPersonMovement>();
     }
 
     void Update()
     {
-        handleInput();
         handleEvents();
-    }
-
-    void handleInput()
-    {
-        if (Input.anyKeyDown)
-        {
-            for (int i = 0; i < KC.acceptableKeys.Length; i++)
-            {
-                if (!Input.GetKeyDown(KC.acceptableKeys[i]))
-                {
-                    unacceptableKeyCounter++;
-                    Debug.Log(unacceptableKeyCounter);
-                }
-            }
-        }
     }
 
     void handleEvents()
     {
-        if (unacceptableKeyCounter == 5 && level == 0)
+        if (p3c.jumpCount >= 25 && level == 0)
         {
-            Debug.Log("asdf");
+            SceneManager.LoadScene("Job"); // REPLACE WITH SCENE
         }
+        switch (level)
+        {
+            case 0:
+                level++;
+                break;
+            case 1:
+                if (player.transform.position.y < 80f)
+                {
+                    level++;
+                    SpawnOnWalledPlatform();
+                }
+                break;
+            case 2:
+                if (player.transform.position.y < 50f)
+                {
+                    level++;
+                    p3c.enabled = false;
+                    p1m.enabled = true;
+                    SpawnInRoom();
+                }
+                break;
+            case 3:
+                if (player.transform.position.y < 0f)
+                {
+                    level++;
+                    SceneManager.LoadScene("Garden");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void SpawnOnWalledPlatform()
+    {
+        platformCamera.enabled = false;
+        platformCamera.GetComponent<AudioListener>().enabled = false;
+        walledCamera.enabled = true;
+        walledCamera.GetComponent<AudioListener>().enabled = true;
+        player.transform.position = new Vector3(0f, 61.5f, 0f);
+        rb.velocity = Vector3.zero;
+    }
+    private void SpawnInRoom()
+    {
+        walledCamera.enabled = false;
+        walledCamera.GetComponent<AudioListener>().enabled = false;
+        fpCamera.enabled = true;
+        fpCamera.GetComponent<AudioListener>().enabled = true;
+        fpCamera.GetComponent<FirstPersonCamera>().enabled = true;
+        player.transform.position = new Vector3(0f, 2f, 0f);
+        rb.velocity = Vector3.zero;
     }
 }
--- Assets/Scripts/Chapter 2/Player3rdPersonController.cs
+++ Assets/Scripts/Chapter 2/Player3rdPersonController.cs
@@ -8,33 +8,26 @@ public class Player3rdPersonController : MonoBehaviour
     public float moveSpeed = 2f;
     public float jumpForce = 1f;
     public float groundDistance = 0.4f;
-    public Camera platformCamera;
-    public Camera walledCamera;
-    public GameObject chapter2Controller;
+    public int jumpCount;
 
     private Rigidbody rb;
     private bool isGrounded;
-    private Chapter2Controller c2c;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        walledCamera.enabled = false;
-        platformCamera.enabled = true;
-        c2c = chapter2Controller.GetComponent<Chapter2Controller>();
     }
 
     private void Update()
     {
         if (rb.transform.position.y == 101.5 || rb.transform.position.y == 51.5)
-        { 
-            isGrounded = true; 
+        {
+            isGrounded = true;
         }
         else
         {
             isGrounded = false;
         }
-
         // Move the player horizontally
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -45,30 +38,7 @@ public class Player3rdPersonController : MonoBehaviour
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpCount++;
         }
-
-        // Check if the player has fallen off the platform
-        if (transform.position.y < 80f && c2c.level == 0)
-        {
-            c2c.level++;
-            SpawnOnWalledPlatform();
-        }
-        if (transform.position.y < 30f && c2c.level == 1)
-        {
-            c2c.level++;
-            SpawnInGarden();
-        }
-    }
-
-    private void SpawnOnWalledPlatform()
-    {
-        platformCamera.enabled = false;
-        walledCamera.enabled = true;
-        transform.position = new Vector3(0f, 61.5f, 0f);
-        rb.velocity = Vector3.zero;
-    }
-    private void SpawnInGarden()
-    {
-        Debug.Log("asdfew");
     }
 }
